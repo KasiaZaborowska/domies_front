9d6a5c3fa19dbc302299c50ac4e05c9d7bc8c17c
Kasia <kasiazet000@gmail.com>
2024-12-15 21:53:21 +0100

    userAccountSlice added

---
+++ src/Store/Redux/userAccountSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { userAccountInterface } from '../../Interfaces';
+
+export const emptyUserState: userAccountInterface = {
+    Email: '',
+    FirstName: '',
+    LastName: '',
+    Password: '',
+    Role: '',
+};
+
+export const userAccountSlice = createSlice({
+    name: 'userAccount',
+    initialState: emptyUserState,
+    reducers: {
+        setLoggedInUser: (state, action) => {
+            state.Email = action.payload.Email;
+            state.FirstName = action.payload.FirstName;
+            state.LastName = action.payload.LastName;
+            state.Password = action.payload.Password;
+            state.Role = action.payload.Role;
+        },
+    },
+});
+
+export const { setLoggedInUser } = userAccountSlice.actions;
+export const userAccountReducer = userAccountSlice.reducer;
