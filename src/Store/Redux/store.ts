5d85a0a4e12fb2cd9e9d23e13bfb7a28d5f73a31
Kasia <kasiazet000@gmail.com>
2024-12-15 21:56:49 +0100

    store update

--- src/Store/Redux/store.ts
+++ src/Store/Redux/store.ts
@@ -1,17 +1,21 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { animalTypeReducer } from './animalTypeSlice';
-import { animalTypeApi } from '../../Apis';
+import { accountApi, animalTypeApi } from '../../Apis';
+import { userAccountReducer } from './userAccountSlice';
 
 const store = configureStore({
     reducer: {
         animalTypeStore: animalTypeReducer,
+        userAccountStore: userAccountReducer,
         [animalTypeApi.reducerPath]: animalTypeApi.reducer,
+        [accountApi.reducerPath]: accountApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(animalTypeApi.middleware),
+        getDefaultMiddleware()
+            .concat(animalTypeApi.middleware)
+            .concat(accountApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
-console.log(store);
