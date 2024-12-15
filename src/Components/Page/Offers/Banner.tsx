12130b4e7df2d8330329ceaf9cf59387243bb470
Kasia <kasiazet000@gmail.com>
2024-12-15 01:13:34 +0100

    added baner to home page

---
+++ src/Components/Page/Offers/Banner.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './banner.css';
+function Banner() {
+    return (
+        <div className="custom-banner">
+            <div
+                className="m-auto d-flex align-items-center"
+                style={{
+                    width: '500px',
+                    height: '65vh',
+                }}
+            >
+                <div
+                    className="d-flex align-items-center"
+                    style={{ width: '100%' }}
+                >
+                    <input
+                        type={'text'}
+                        className="form-control rounded-pill"
+                        style={{
+                            width: '100%',
+                            padding: '20px 20px',
+                        }}
+                        placeholder="Search"
+                    />
+                    <span style={{ position: 'relative', left: '-43px' }}>
+                        <i className="bi bi-search"></i>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
