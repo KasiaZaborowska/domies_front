6c21c290787d662653f504769c720a948e4c8a34
Kasia <kasiazet000@gmail.com>
2025-01-12 23:50:34 +0100

    store update

--- src/Store/Redux/store.ts
+++ src/Store/Redux/store.ts
@@ -1,23 +1,27 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { animalTypeReducer } from './animalTypeSlice';
-import { accountApi, animalTypeApi, offerApi } from '../../Apis';
+import { accountApi, animalApi, animalTypeApi, offerApi } from '../../Apis';
 import { userAccountReducer } from './userAccountSlice';
 import { offerReducer } from './offerSlice';
+import { animalReducer } from './animalSlice';
 
 const store = configureStore({
     reducer: {
         animalTypeStore: animalTypeReducer,
         userAccountStore: userAccountReducer,
         offerStore: offerReducer,
+        animalStore: animalReducer,
         [offerApi.reducerPath]: offerApi.reducer,
         [animalTypeApi.reducerPath]: animalTypeApi.reducer,
         [accountApi.reducerPath]: accountApi.reducer,
+        [animalApi.reducerPath]: animalApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware()
             .concat(animalTypeApi.middleware)
             .concat(accountApi.middleware)
-            .concat(offerApi.middleware),
+            .concat(offerApi.middleware)
+            .concat(animalApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
