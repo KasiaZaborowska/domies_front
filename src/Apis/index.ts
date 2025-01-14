3281d5ac8602669b061cb3890b6f55a09c1e5696
Kasia <kasiazet000@gmail.com>
2025-01-14 15:49:10 +0100

    small update

--- src/Apis/index.ts
+++ src/Apis/index.ts
@@ -2,5 +2,6 @@ import animalTypeApi from './animalTypeApi';
 import accountApi from './accountApi';
 import offerApi from './offerApi';
 import animalApi from './animalApi';
+import applicationApi from './applicationApi';
 
-export { animalTypeApi, accountApi, offerApi, animalApi };
+export { animalTypeApi, accountApi, offerApi, animalApi, applicationApi };
