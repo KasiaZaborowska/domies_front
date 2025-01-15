a70e9d2011fb1de56e2d2334815111e4336302c0
Kasia <kasiazet000@gmail.com>
2025-01-15 22:18:00 +0100

    deafult Handle Delete

---
+++ src/Pages/Applications/components/HandleDelete.tsx
@@ -0,0 +1,85 @@
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
+import React from 'react';
+import { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+interface DeleteButtonWithModalProps {
+    id: number;
+    deleteFunction: (
+        id: number,
+    ) => Promise<
+        { data: any } | { error: FetchBaseQueryError | SerializedError }
+    >;
+}
+
+function DeleteButtonWithModal({
+    id,
+    deleteFunction,
+}: DeleteButtonWithModalProps) {
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+    const handleConfirmDelete = async () => {
+        console.log('ID: ', id);
+        if (id) {
+            try {
+                console.log(typeof id);
+                await deleteFunction(id);
+                setShowDeleteModal(false);
+                //window.location.reload();
+            } catch (error) {
+                console.error('Błąd podczas usuwania:', error);
+            }
+        } else {
+            console.error('Brak ID do usunięcia.');
+        }
+    };
+
+    const handleCloseModal = () => {
+        setShowDeleteModal(false);
+    };
+
+    return (
+        <>
+            <>
+                <Modal
+                    show={showDeleteModal}
+                    onHide={() => setShowDeleteModal(false)}
+                    centered
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Confirm Delete</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Czy napewno chcesz usunąć ten element? Nie da się tego
+                        cofnąć.
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleCloseModal}>
+                            Zamknij
+                        </Button>
+                        <Button variant="danger" onClick={handleConfirmDelete}>
+                            {'Usuń'}
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+                <Button
+                    style={{
+                        marginRight: 8,
+                        paddingLeft: 20,
+                        paddingRight: 20,
+                    }}
+                    variant="contained"
+                    color="error"
+                    className="btn btn-danger mx-2"
+                    //onClick={() => onEdit(params.row)}
+                    onClick={() => setShowDeleteModal(true)}
+                >
+                    <i className="bi bi-trash-fill"></i>
+                </Button>{' '}
+            </>
+        </>
+    );
+}
+
+export default DeleteButtonWithModal;
