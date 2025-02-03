bbb5e18b7398bc05eb3d7093f2597ef8c1417eaa
Kasia <kasiazet000@gmail.com>
2025-02-03 23:28:16 +0100

    opinion Interface created

---
+++ src/Interfaces/opinionInterface.ts
@@ -0,0 +1,10 @@
+import animalInterface from './animalInterface';
+
+export default interface opinionInterface {
+    id?: number;
+    rating: number;
+    comment: string;
+    applicationId: number;
+    userEmail: string;
+    opinionDateAdd: string;
+}
