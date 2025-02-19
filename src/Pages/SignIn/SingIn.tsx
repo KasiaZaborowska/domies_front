8d5d3a61ae71c62965411d821245e7cff0594942
Kasia <kasiazet000@gmail.com>
2025-02-19 17:47:19 +0100

    SignIn with errorMessage and validation

--- src/Pages/SignIn/SingIn.tsx
+++ src/Pages/SignIn/SingIn.tsx
@@ -3,22 +3,18 @@ import inputHelper from '../../Helper/inputHelper';
 import { useLoginUserMutation } from '../../Apis/accountApi';
 import { userAccountInterface } from '../../Interfaces';
 import jwt_decode from 'jwt-decode';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setLoggedInUser } from '../../Store/Redux/userAccountSlice';
-import { RootState } from '../../Store/Redux/store';
+import { Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import MainLoader from '../../Components/MainLoader';
 import toastNotify from '../../Helper/toastNotify';
 
 function SingIn() {
     const dispatch = useDispatch();
-    const [error, setError] = useState('');
+    const [errorMessage, setErrorMessage] = useState<string>('');
+    const [validated, setValidated] = useState(false);
     const [loginUser] = useLoginUserMutation();
-
-    // const userAccountStore = useSelector(
-    //     (state: RootState) => state.userAccountStore,
-    // );
-    // console.log('userAccountStore ', userAccountStore);
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
     const [userInput, setUserInput] = useState({
@@ -33,44 +29,79 @@ function SingIn() {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        const form = e.currentTarget;
+
+        if (form.checkValidity() === false) {
+            e.stopPropagation();
+            setValidated(true);
+            console.log('is  false ! ');
+            // return;
+        }
         setLoading(true);
-        const response: any = await loginUser({
-            Email: userInput.Email,
-            Password: userInput.Password,
-        });
+        try {
+            const response: any = await loginUser({
+                Email: userInput.Email,
+                Password: userInput.Password,
+            }).unwrap();
 
-        console.log(response);
+            console.log(response);
+            console.log(response.token);
 
-        if (response.data) {
-            console.log('if, data:', response);
-            const token = response.data.token;
-            console.log(token);
-            const { Email, FirstName, LastName, Role }: userAccountInterface =
-                jwt_decode(token);
-            localStorage.setItem('token', token);
-            dispatch(setLoggedInUser({ Email, FirstName, LastName, Role }));
-            console.log(setLoggedInUser({ Email, FirstName, LastName, Role }));
+            if (response) {
+                console.log('if, data:', response);
+                const token = response.token;
+                console.log(token);
+                const {
+                    Email,
+                    FirstName,
+                    LastName,
+                    Role,
+                }: userAccountInterface = jwt_decode(token);
+                console.log(' przed token ustawiam');
+                localStorage.setItem('token', token);
+                console.log('token ustawiam');
+                dispatch(setLoggedInUser({ Email, FirstName, LastName, Role }));
+                console.log(
+                    setLoggedInUser({ Email, FirstName, LastName, Role }),
+                );
 
-            navigate('/');
-            toastNotify('Logowanie zakończone sukcesem.');
-        } else if (response.error) {
-            console.log('error:', response.error.data.message);
-            //setError(response.error.data.message);
-            toastNotify(response.error.data.message, 'error');
+                navigate('/');
+                toastNotify('Logowanie zakończone sukcesem.');
+            }
+        } catch (error: any) {
+            console.error('Błąd przy dodawaniu:', error.data?.errors);
+            if (error?.data?.errors) {
+                setErrorMessage(error.data.errors);
+            } else {
+                console.log('error:', error.data.message);
+                toastNotify(error.data.message, 'error');
+                setLoading(false);
+                setValidated(false);
+                return;
+            }
         }
 
         setLoading(false);
+        setValidated(true);
     };
-
+    function getErrorMessage(key: any) {
+        return errorMessage.hasOwnProperty(key) ? errorMessage[key][0] : '';
+    }
     return (
         <div className="container text-center">
             {loading && <MainLoader />}
-            <form method="post" onSubmit={handleSubmit}>
+            <Form
+                method="post"
+                onSubmit={handleSubmit}
+                noValidate
+                validated={validated}
+            >
                 <h1 className="mt-5">Login</h1>
                 <div className="mt-5" style={{ minWidth: '40vw' }}>
                     <div className="mt-4">
                         <input
-                            type="text"
+                            type="email"
                             className="form-control"
                             placeholder="Enter Username"
                             required
@@ -78,6 +109,9 @@ function SingIn() {
                             value={userInput.Email}
                             onChange={handleUserInput}
                         />
+                        <div className="invalid-feedback">
+                            {getErrorMessage('Email')}
+                        </div>
                     </div>
 
                     <div className="mt-4">
@@ -86,15 +120,18 @@ function SingIn() {
                             className="form-control"
                             placeholder="Enter Password"
                             required
+                            minLength={8}
                             name="Password"
                             value={userInput.Password}
                             onChange={handleUserInput}
                         />
+                        <div className="invalid-feedback">
+                            {getErrorMessage('Password')}
+                        </div>
                     </div>
                 </div>
 
                 <div className="mt-4">
-                    {error && <p className="text-danger">{error}</p>}
                     <button
                         type="submit"
                         className="btn"
@@ -108,7 +145,7 @@ function SingIn() {
                         Login
                     </button>
                 </div>
-            </form>
+            </Form>
         </div>
     );
 }
