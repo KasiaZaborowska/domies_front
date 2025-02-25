4f25831c5dc0409176fe4e68fc992f381e05a1d9
Kasia <kasiazet000@gmail.com>
2025-02-25 20:25:12 +0100

    phone number added

--- src/Interfaces/userAccountInterface.ts
+++ src/Interfaces/userAccountInterface.ts
@@ -2,6 +2,7 @@ export default interface userAccountInterface {
     Email: string;
     FirstName: string;
     LastName: string;
+    PhoneNumber?: string;
     Password: string;
     Role: string;
 }
