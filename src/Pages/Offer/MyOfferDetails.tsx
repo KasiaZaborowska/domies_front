ab66e2afbe4cf08c15aacae48eaa016c78793944
Kasia <kasiazet000@gmail.com>
2025-03-05 22:53:46 +0100

    fix display price in myOfferDetails

--- src/Pages/Offer/MyOfferDetails.tsx
+++ src/Pages/Offer/MyOfferDetails.tsx
@@ -375,7 +375,11 @@ function MyOfferDetails() {
                                     />
                                 </div>
                                 <span className="h3">
-                                    Koszt usługi: {data.result.price}zł /24h
+                                    Koszt usługi:{' '}
+                                    {data.result.price
+                                        .toString()
+                                        .replace('.', ',')}
+                                    zł / 24h
                                 </span>
                                 <hr />
                                 <div className="row pt-4">
