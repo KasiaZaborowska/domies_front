0d7e207664b9c70b3ea288c2561e2b81ab332fae
Kasia <kasiazet000@gmail.com>
2025-03-16 14:17:45 +0100

    clean up

--- src/Pages/AuthTest/index.ts
+++
@@ -1,4 +0,0 @@
-import AccessDenied from './AccessDenied';
-import AuthenticationTest from './AuthenticationTest';
-import AuthenticationTestAdmin from './AuthenticationTestAdmin';
-export { AccessDenied, AuthenticationTest, AuthenticationTestAdmin };
